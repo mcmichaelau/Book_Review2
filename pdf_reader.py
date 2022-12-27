93f97f5804f78d8e5f40d6f79de47ac1cade9989
mcmichaelau <austintm2@gmail.com>
2022-12-27 04:24:48 -0500

    test 12

--- pdf_reader.py
+++ pdf_reader.py
@@ -49,7 +49,7 @@ def pdf_to_text(file, split_length):
         else:
             split_text_list.append(f' {text_list[i]}')
 
-# text
+# tex
 
     for i in range(len(split_text_list)):
         # print(repr(split_text_list[i]))
