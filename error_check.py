2d6f51f98455ad92a67d4a1e524e38deb0b1aa04
mcmichaelau <austintm2@gmail.com>
2022-12-30 02:13:39 -0500

    Added maturity content filter and a sleep for when server overload error occurs in spell check

--- error_check.py
+++ error_check.py
@@ -1,6 +1,8 @@
 from pdf_reader import pdf_to_text
 from gingerit.gingerit import GingerIt
 import pdfquery
+from better_profanity import profanity
+import time
 
 
 def collect_errors(file):
@@ -18,6 +20,8 @@ def collect_errors(file):
 
     error_list = []
 
+    mature_list = []
+
     # loop through each page of the pdf
     for i in range(1, pdf.doc.catalog['Pages'].resolve()['Count'] + 1):
 
@@ -33,7 +37,17 @@ def collect_errors(file):
             # replace line breaks with a space
             sentence = sentence.replace('\n', ' ')
 
-            errors = parser.parse(sentence)
+            # check for mature content
+            mature = profanity.contains_profanity(sentence)
+
+            if mature:
+                mature_list.append(f'Page{i}:{sentence}')
+
+            try:
+                errors = parser.parse(sentence)
+            except:
+                time.sleep(600)
+                errors = parser.parse(sentence)
 
             if errors is None:
                 pass
@@ -53,7 +67,21 @@ def collect_errors(file):
                 except:
                     pass
 
-    return error_list
+    return error_list, mature_list
+
+
+def sort_mature(mature_list, filename):
+    for item in mature_list:
+        print(item)
+        print('Keep? (y/n)')
+        # create text input
+        keep = input()
+        if keep == 'y':
+            # append error to text file
+            with open(f'{filename}_mature.txt', 'a') as f:
+                f.write(f"{item}\n")
+        elif keep == 'n':
+            pass
 
 
 def sort_errors(error_list, filename):
@@ -69,4 +97,3 @@ def sort_errors(error_list, filename):
                 f.write(f"{error}\n")
         elif keep == 'n':
             pass
-
